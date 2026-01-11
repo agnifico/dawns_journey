4198be9e12375a14fbe1524d0385e8a7d50ad11f
Agni Bhattacharya <agnx13@proton.me>
2026-01-12 03:20:13 +0530

    Gallery, 3 Towers v2

--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,18 @@
 import adapter from '@sveltejs/adapter-vercel';
 
+const npcIds = ['akari', 'claudia', 'cygwin', 'guinevere', 'hanabi', 'hela', 'marjane', 'nyx', 'sylvie', 'veres'];
+const characterJournalEntries = npcIds.map(id => `/journal/character/${id}`);
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter()
+		adapter: adapter(),
+		prerender: {
+			entries: [
+				'*', // Prerender all static pages
+				...characterJournalEntries
+			]
+		}
 	}
 };
 
